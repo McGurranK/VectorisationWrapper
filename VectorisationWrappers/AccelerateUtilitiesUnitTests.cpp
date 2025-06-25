376feb645d04cfa4220144219f7c94427b682237
Kristian McGurran <kmcgurran123@hotmail.com>
2025-06-25 19:14:05 +0100

    Adding test checks

--- VectorisationWrappers/AccelerateUtilitiesUnitTests.cpp
+++ VectorisationWrappers/AccelerateUtilitiesUnitTests.cpp
@@ -6,9 +6,13 @@ TEMPLATE_TEST_CASE("Accelerate Ranges Unit Tests", "[Accelerate Ranges]", float,
 {
     using T = TestType;
 
-    std::vector<T> inputBuffer;
-    std::vector<T> outputBuffer;
+    constexpr auto bufferSize = 1024;
 
+    std::vector<T> inputBuffer (bufferSize);
+    std::vector<T> outputBuffer (bufferSize);
+
+    inputBuffer.assign (bufferSize, static_cast<T>(0));
+    outputBuffer.assign (bufferSize, static_cast<T>(2));
 
 
     SECTION("Accelerate Abs Values")
@@ -17,17 +21,26 @@ TEMPLATE_TEST_CASE("Accelerate Ranges Unit Tests", "[Accelerate Ranges]", float,
     SECTION ("Accelerate Fill Buffer")
     {
         T valueToFill = 1.0;
-        Vectorised::Utilities::fillBuffer (&valueToFill, inputBuffer.data(), 256);
+        Vectorised::Utilities::fillBuffer (&valueToFill, inputBuffer.data(), bufferSize);
+
+        for (const auto& Value : inputBuffer)
+        {
+            REQUIRE (Value == valueToFill);
+        }
     }
 
     SECTION ("Accelerate Zero Buffer")
     {
+        Vectorised::Utilities::zeroBuffer (outputBuffer.data(), bufferSize);
 
-        Vectorised::Utilities::zeroBuffer (inputBuffer.data(), 256);
+        for (const auto& Value : inputBuffer)
+        {
+            REQUIRE (Value == static_cast<T>(0));
+        }
     }
 
     SECTION ("Accelerate Scale Buffer")
     {
-        Vectorised::Utilities::scalerMultiplication (inputBuffer.data(), outputBuffer.data(), 256);
+        // Vectorised::Utilities::scalerMultiplication (inputBuffer.data(), outputBuffer.data(), 256);
     }
 }
