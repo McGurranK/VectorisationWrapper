aaf8be7579ee667bc7263df64eee053b0e27608f
Kristian McGurran <kmcgurran123@hotmail.com>
2025-06-26 13:11:20 +0100

    Basic Vectorised Arithmetic

--- VectorisationWrappers/AccelerateWrapperUnitTests.cpp
+++ VectorisationWrappers/AccelerateWrapperUnitTests.cpp
@@ -3,50 +3,88 @@
 #include <Accelerate/Accelerate.h>
 #include "Accelerate/AccelerateWrapper.h"
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_template_test_macros.hpp>
 
-TEST_CASE("VDSP wrapper testing", "[VDSP Wrapper]")
+
+TEMPLATE_TEST_CASE ("VDSP wrapper testing", "[VDSP Wrapper]", float, double)
 {
-    std::vector<float> input (256);
-    std::vector<float> output (256);
+    using T = TestType;
 
-    input.assign (256, 0.0f);
-    output.assign (256, 1.0f);
+    constexpr auto bufferSize = 1024;
+
+    std::vector<T> input (bufferSize);
+    std::vector<T> output (bufferSize);
+
+    input.assign (bufferSize, 0.0f);
+    output.assign (bufferSize, 1.0f);
 
     SECTION ("Addition Test")
     {
-        Vectorised::additionVectorised (input.data(), output.data(), 256);
+        Vectorised::additionVectorised (input.data(), output.data(), bufferSize);
+
+        for (const auto& value : output)
+        {
+            REQUIRE (value == static_cast<T>(1));
+        }
     }
 
     SECTION ("Subtraction Test")
     {
-        Vectorised::subtractionVectorised (input.data(), output.data(), 256);
+        input.assign (bufferSize, 5.0f);
+        output.assign (bufferSize, 4.0f);
+
+        // Todo: This call order is confusing
+        Vectorised::subtractionVectorised (output.data(), input.data(), bufferSize);
+
+        for (const auto& value : output)
+        {
+            REQUIRE (value == static_cast<T>(1));
+        }
     }
 
     SECTION ("Multiplication Test")
     {
-       Vectorised::multiplicationVectorised (input.data(), output.data(), 256);
+        input.assign (bufferSize, 2.0f);
+        output.assign (bufferSize, 1.0f);
+
+        // Todo: Order is confusing
+       Vectorised::multiplicationVectorised (input.data(), output.data(), bufferSize);
+
+        for (const auto& value : input)
+        {
+            REQUIRE (value == static_cast<T>(2));
+        }
     }
 
     SECTION ("Division Test")
     {
-        Vectorised::divisionVectorised (input.data(), output.data(), 256);
-    }
+        input.assign (bufferSize, 2.0f);
+        output.assign (bufferSize, 2.0f);
 
-    SECTION ("Division Test")
-    {
-        Vectorised::tanhVectorised (input.data(), output.data(), 256);
-    }
+        // Todo: Function input confusing
+        Vectorised::divisionVectorised (input.data(), output.data(), bufferSize);
 
-    SECTION ("Division Test")
-    {
-        Vectorised::sineVectorised (input.data(), 256);
+        for (const auto& value : input)
+        {
+            REQUIRE (value == static_cast<T>(1));
+        }
     }
-
-    SECTION ("Division Test")
-    {
-        const auto lowerLimit = -1.f;
-        const auto upperLimit = -1.f;
-
-        Vectorised::hardClipVectorised (input.data(), output.data(), 256, &lowerLimit, &upperLimit);
-    }
+    
+    // SECTION ("Division Test")
+    // {
+    //     Vectorised::tanhVectorised (input.data(), output.data(), 256);
+    // }
+    //
+    // SECTION ("Division Test")
+    // {
+    //     Vectorised::sineVectorised (input.data(), 256);
+    // }
+    //
+    // SECTION ("Division Test")
+    // {
+    //     const auto lowerLimit = -1.f;
+    //     const auto upperLimit = -1.f;
+    //
+    //     Vectorised::hardClipVectorised (input.data(), output.data(), 256, &lowerLimit, &upperLimit);
+    // }
 }
