cd86cb846e9d58c5888590c3df59f82757db25e7
Kristian McGurran <kmcgurran123@hotmail.com>
2025-12-07 13:02:13 +0000

    Updating & cleaning

--- VIVIAudioBasics/Buffering/VIVIAudioBuffer.h
+++ VIVIAudioBasics/Buffering/VIVIAudioBuffer.h
@@ -3,12 +3,17 @@
 
 namespace VIVI::AudioBasics
 {
+    template <std::floating_point SampleType>
     class VIVIAudioBuffer final
     {
     public:
-        VIVIAudioBuffer() = default;
+        VIVIAudioBuffer (SampleType** SampleData, int NumChannels, int NumSamples)
+        {
+            channelPointer.prepare (SampleData, NumChannels);
+        }
 
 
     private:
+        VIVIChannelPtr<SampleType> channelPointer;
     };
 }
--- VIVIAudioBasics/Buffering/VIVIChannelPtr.cpp
+++ VIVIAudioBasics/Buffering/VIVIChannelPtr.cpp
@@ -4,10 +4,10 @@ SCENARIO("VIVI channel pointer", "[Normal BS]")
     GIVEN("Buffer")
     {
         VIVI::AudioBasics::VIVIChannelPtr<float> channelPtr;
+        std::vector<float> values { 1.f, 2.f, 3.f, 4.f };
 
         WHEN("Preparing with float** context")
         {
-            std::vector<float> values { 1.f, 2.f, 3.f, 4.f };
             std::vector<float*> channelPointer { &values[0], &values[1] };
 
             THEN("Success")
@@ -21,7 +21,6 @@ SCENARIO("VIVI channel pointer", "[Normal BS]")
 
         WHEN ("Gave invalid context")
         {
-            std::vector<float> values { 1.f, 2.f, 3.f, 4.f };
             std::vector<float*> channelPointer { &values[0], nullptr };
 
             THEN("Success")
@@ -32,36 +31,3 @@ SCENARIO("VIVI channel pointer", "[Normal BS]")
     }
 
 }
-
-
-
-
-// TEMPLATE_TEST_CASE ("Channel Pointer Testing", "[VIVI Pointer]", float, double)
-// {
-//     VIVI::AudioBasics::VIVIChannelPtr<TestType> channelPtr;
-//
-//     // std::vector<t> typeBuffer;
-//     // typeBuffer.push_back(1);
-//     // typeBuffer.push_back(2);
-//     // typeBuffer.push_back(3);
-//     //
-//     // SECTION ("preparing channel pointers")
-//     // {
-//     //     // Needs a runtime check and throw
-//     //     std::vector<t*> channel;
-//     //     channel.push_back(&typeBuffer[0]);
-//     //     channel.push_back(&typeBuffer[1]);
-//     //     channel.push_back(&typeBuffer[2]);
-//     //
-//     //     channelPtr.prepare(channel);
-//     //
-//     //     const auto numberOfChannels = channelPtr.getNumberChannels();
-//     //
-//     //     REQUIRE (numberOfChannels == 3);
-//     //     REQUIRE (*channelPtr[0] == static_cast<t>(1));
-//     //     REQUIRE (*channelPtr[1] == static_cast<t>(2));
-//     //     REQUIRE (*channelPtr[2] == static_cast<t>(3));
-//     //     REQUIRE (channelPtr[0][1] == static_cast<t>(2));
-//     //     REQUIRE (channelPtr[0][2] == static_cast<t>(3));
-//     // }
-// }
